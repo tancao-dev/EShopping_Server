4228ea78319d3efc866eff8c89e4be1ad59b4d62
tan.cao <tan.cao@vietunion.com.vn>
2020-08-25 14:09:58 +0700

    abc

--- controller/controller.js
+++ controller/controller.js
@@ -4,20 +4,127 @@ var rawdata = fs.readFileSync("./query/queries.json");
 var queries = JSON.parse(rawdata);
 
 class MainController {
-  async getAllData(req, res) {
+  // ============= For admin =============
+  async getListUsers(req, res) {
     try {
       const pool = await poolPromise;
-      const result = await pool.request().query(queries.getAllData);
+      const result = await pool.request().query(queries.getListUsers);
       res.json(result.recordset);
     } catch (error) {
       res.status(500);
       res.send(error.message);
     }
   }
+  async getListProducts(req, res) {
+    try {
+      const pool = await poolPromise;
+      const result = await pool.request().query(queries.getListProducts);
+      res.json(result.recordset);
+    } catch (error) {
+      res.status(500);
+      res.send(error.message);
+    }
+  }
+  //maso,ten,nhasanxuat,hinhanh,mota,thongso,giabanhienhanh,idnhomsanpham
+  async postAddProduct(req, res) {
+    try {
+      if (
+        req.body.ten != null &&
+        req.body.nhasanxuat != null &&
+        req.body.hinhanh != null &&
+        req.body.mota != null &&
+        req.body.thongso != null &&
+        req.body.giabanhienhanh != null &&
+        req.body.idnhomsanpham != null
+      ) {
+        const pool = await poolPromise;
+        const result = await pool
+          .request()
+          .input("maso", sql.VarChar, req.body.maso)
+          .input("ten", sql.NVarChar, req.body.ten)
+          .input("nhasanxuat", sql.NVarChar, req.body.nhasanxuat)
+          .input("hinhanh", sql.VarChar, req.body.hinhanh)
+          .input("mota", sql.NVarChar, req.body.mota)
+          .input("thongso", sql.NVarChar, req.body.thongso)
+          .input("giabanhienhanh", sql.Float, req.body.giabanhienhanh)
+          .input("tinhtrang", sql.SmallInt, req.body.tinhtrang)
+          .query(queries.addProduct);
+        res.json(result);
+      } else {
+        res.send("Please fill all the details!");
+      }
+    } catch (error) {
+      res.status(500);
+      res.send(error.message);
+    }
+  }
+
+  async postUpdateProduct(req, res) {
+    try {
+      if ( req.body.ten != null &&
+        req.body.nhasanxuat != null &&
+        req.body.hinhanh != null &&
+        req.body.mota != null &&
+        req.body.thongso != null &&
+        req.body.giabanhienhanh != null &&
+        req.body.idnhomsanpham != null) {
+        const pool = await poolPromise;
+        const result = await pool
+        .request()
+        .input("maso", sql.VarChar, req.body.maso)
+        .input("ten", sql.NVarChar, req.body.ten)
+        .input("nhasanxuat", sql.NVarChar, req.body.nhasanxuat)
+        .input("hinhanh", sql.VarChar, req.body.hinhanh)
+        .input("mota", sql.NVarChar, req.body.mota)
+        .input("thongso", sql.NVarChar, req.body.thongso)
+        .input("giabanhienhanh", sql.Float, req.body.giabanhienhanh)
+        .input("tinhtrang", sql.SmallInt, req.body.tinhtrang)
+        .query(queries.updateProduct);
+        res.json(result);
+      } else {
+        res.send("All fields are required!");
+      }
+    } catch (error) {
+      res.status(500);
+      res.send(error.message);
+    }
+  }
+
+  async postDeleteProduct(req, res) {
+    try {
+      if (req.body.id != null) {
+        const pool = await poolPromise;
+        const result = await pool
+          .request()
+          .input("id", sql.Int, req.body.id)
+          .query(queries.deleteProduct);
+        res.json(result);
+      } else {
+        res.send("Please fill all the details!");
+      }
+    } catch (error) {
+      res.status(500);
+      res.send(error.message);
+    }
+  }
+
+  // ============= For general =============
   async getAllCategory(req, res) {
     try {
       const pool = await poolPromise;
       const result = await pool.request().query(queries.getAllCategory);
+      res.json(result.recordset);
+    } catch (error) {
+      res.status(500);
+      res.send(error.message);
+    }
+  }
+
+  // ============= For user =============
+  async getAllData(req, res) {
+    try {
+      const pool = await poolPromise;
+      const result = await pool.request().query(queries.getAllData);
       res.json(result.recordset);
     } catch (error) {
       res.status(500);
@@ -70,7 +177,7 @@ class MainController {
   }
   //hoten,ngaysinh,CMND,diachi,tendangnhap,matkhau,email,vaitro,tinhtrang
   // 1 ad 0 cus 1 active 0 inactive
-  async addNewData(req, res) {
+  async signup(req, res) {
     try {
       if (
         req.body.CMND != null &&
--- routes/route.js
+++ routes/route.js
@@ -2,16 +2,26 @@ const express =  require('express');
 const controller = require('../controller/controller')
 
 const router = express.Router();
+
+// For admin
+router.get('/api/admin/getListUsers', controller.getListUsers);
+router.get('/api/admin/getListProducts', controller.getListProducts);
+router.post('/api/admin/addProduct' , controller.postAddProduct);
+router.post('/api/admin/updateProduct',controller.postUpdateProduct);
+router.post('/api/admin/deleteProduct' , controller.postDeleteProduct);
+// router.get('/api/user/getAllData', controller.getAllData);
+
+// For user
 router.get('/api/user/getAllData', controller.getAllData);
 router.post('/api/user/login', controller.login);
 router.post('/api/user/cart',controller.getShoppingCart);
-
+router.post('/api/user/signup', controller.signup)
 
 // General
 router.get('/api/general/categories', controller.getAllCategory);
 // router.post('/api/signup',controller.signup);
-router.post('/api/addNewData' , controller.addNewData);
-router.put('/api/updateData',controller.updateData);
-router.delete('/api/deleteData' , controller.deleteData);
+// router.post('/api/addNewData' , controller.addNewData);
+// router.put('/api/updateData',controller.updateData);
+// router.delete('/api/deleteData' , controller.deleteData);
 
 module.exports = router;
